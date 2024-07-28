9c7f999d4fde675da5436b8f0a9fd2507c74aac1
NikaApts1auri <nika.aptsiauri77@gmail.com>
2024-07-28 18:19:24 +0400

    Style: fix styles

--- Script.js
+++ Script.js
@@ -22,70 +22,15 @@ container.addEventListener("click", (event) => {
 
 //slider
 
-const slider = document.querySelector(".slider");
-const indicator = document.querySelector(".indicator");
-const slideWidth =
-  document.querySelector(".slide").offsetWidth +
-  parseFloat(getComputedStyle(slider).gap);
 
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener("mousedown", (e) => {
-  isDown = true;
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
 
-slider.addEventListener("mouseleave", () => {
-  if (isDown) {
-    slideToNearestSlide();
-  }
-  isDown = false;
-});
 
-slider.addEventListener("mouseup", () => {
-  if (isDown) {
-    slideToNearestSlide();
-  }
-  isDown = false;
-  console.log(isDown);
-});
 
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
 
-  let currentX = e.pageX - slider.offsetLeft;
-  let walk = currentX - startX;
-
-  slider.scrollLeft = scrollLeft - walk;
-  updateIndicator();
-});
-
-function updateIndicator() {
-  const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-  const percentageScrolled = (slider.scrollLeft / maxScrollLeft) * 100;
-  const indicatorWidth = (indicator.clientWidth / slider.clientWidth) * 100;
-  const leftPosition = (percentageScrolled * (100 - indicatorWidth)) / 100;
-
-  indicator.style.left = `${leftPosition}%`;
-}
-
-function slideToNearestSlide() {
-  const currentScroll = slider.scrollLeft;
-
-  const exactScrollPosition = currentScroll / slideWidth;
-  const currentSlideIndex = Math.round(exactScrollPosition);
-
-  const newScrollLeft = currentSlideIndex * slideWidth;
-
-  slider.scrollTo({
-    left: newScrollLeft,
-    behavior: "smooth",
-  });
-}
 // menu trigger
 
 const menuTrigger=document.querySelector(".menu-trigger")
@@ -94,7 +39,20 @@ const threePoint=document.querySelector(".threePoint")
 const x=document.querySelector(".x")
 
 
-
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 menuTrigger.addEventListener('click', () => {
     if (menuButtons.style.display === 'flex') {
         menuButtons.style.display = 'none';
